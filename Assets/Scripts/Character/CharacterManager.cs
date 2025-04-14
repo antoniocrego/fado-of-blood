1a1e37334e0d92a5c9355fbc83c177e5bcf88482
Rodrigo Campos Rodrigues <rodrigues.rodrigo2003@gmail.com>
2025-04-14 19:22:51 +0100

    build: 🚧 effects logic. It is working for the stamina. However, couldn't really test for the health damage because for some reason the collision between the player and the wall of pain is not being detected.

--- Assets/Scripts/Character/CharacterManager.cs
+++ Assets/Scripts/Character/CharacterManager.cs
@@ -4,18 +4,17 @@ public class CharacterManager : MonoBehaviour
 {
     public CharacterInventoryManager inventoryManager;
     public CharacterEquipmentManager equipmentManager;
-    // public CharacterStatsManager statsManager;
-    // public CharacterAnimatorManager animatorManager;
-    // public CharacterEffectsManager effectsManager;
-    // public CharacterAudioManager audioManager;
+    [HideInInspector] public CharacterEffectsManager characterEffectsManager;
+    public float health;
+    public float stamina;
+    public bool isDead = false;
 
     protected virtual void Awake()
     {
         inventoryManager = GetComponent<CharacterInventoryManager>();
         equipmentManager = GetComponent<CharacterEquipmentManager>();
-        // statsManager = GetComponent<CharacterStatsManager>();
-        // animatorManager = GetComponent<CharacterAnimatorManager>();
-        // effectsManager = GetComponent<CharacterEffectsManager>();
-        // audioManager = GetComponent<CharacterAudioManager>();
+        characterEffectsManager = GetComponent<CharacterEffectsManager>();
+        health = 100;
+        stamina = 100;
     }
 }
