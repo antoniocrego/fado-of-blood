0ad3b87dfda94047342423874215948ff7ec4be0
Luis Relvas <up202108661@up.pt>
2025-05-21 15:53:08 +0100

    started worked on the inventory interface without any logic behind

--- Assets/Scripts/UI/PlayerUIHudManager.cs
+++ Assets/Scripts/UI/PlayerUIHudManager.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUIHudManager : MonoBehaviour
 {
 
-    [SerializeField] UiStat_Bar healthBar; 
-    [SerializeField] UiStat_Bar staminaBar; 
-    
-    public void SetNewHealthValue(float newValue) 
+    [SerializeField] UiStat_Bar healthBar;
+    [SerializeField] UiStat_Bar staminaBar;
+
+    [SerializeField] Image rightWeaponQuickSlotIcon;
+    [SerializeField] Image leftWeaponQuickSlotIcon;
+
+
+    public void SetNewHealthValue(float newValue)
     {
         healthBar.SetStat(newValue);
-    }   
+    }
 
-    public void SetMaxHealthValue(int newValue) 
+    public void SetMaxHealthValue(int newValue)
     {
         healthBar.SetMaxStat(newValue);
     }
@@ -19,10 +24,60 @@ public class PlayerUIHudManager : MonoBehaviour
     public void SetNewStaminaValue(float newValue)
     {
         staminaBar.SetStat(newValue);
-    }   
+    }
 
-    public void SetMaxStaminaValue(int newValue) 
+    public void SetMaxStaminaValue(int newValue)
     {
         staminaBar.SetMaxStat(newValue);
     }
+
+
+    public void SetRightWeaponQuickSlotIcon(int weaponID)
+    {
+        WeaponItem weapon = WorldItemDatabase.Instance.GetWeaponByID(weaponID);
+
+        if (weapon == null)
+        {
+            Debug.Log("Weapon is null");
+            rightWeaponQuickSlotIcon.enabled = false;
+            rightWeaponQuickSlotIcon.sprite = null;
+            return;
+        }
+
+        if (weapon.itemIcon == null)
+        {
+            Debug.Log("Weapon icon is null");
+            rightWeaponQuickSlotIcon.enabled = false;
+            rightWeaponQuickSlotIcon.sprite = null;
+            return;
+        }
+
+        rightWeaponQuickSlotIcon.sprite = weapon.itemIcon;
+        rightWeaponQuickSlotIcon.enabled = true;
+    }
+
+    public void SetLeftWeaponQuickSlotIcon(int weaponID)
+    {
+        WeaponItem weapon = WorldItemDatabase.Instance.GetWeaponByID(weaponID);
+
+        if (weapon == null)
+        {
+            Debug.Log("Weapon is null");
+            leftWeaponQuickSlotIcon.enabled = false;
+            leftWeaponQuickSlotIcon.sprite = null;
+            return;
+        }
+
+        if (weapon.itemIcon == null)
+        {
+            Debug.Log("Weapon icon is null");
+            leftWeaponQuickSlotIcon.enabled = false;
+            leftWeaponQuickSlotIcon.sprite = null;
+            return;
+        }
+
+        leftWeaponQuickSlotIcon.sprite = weapon.itemIcon;
+        leftWeaponQuickSlotIcon.enabled = true;
+    }
+
 }
