b4f29f6356227469dfc6c737973bde7f94bc324c
António Rego <antoniocostarego618@gmail.com>
2025-06-11 18:31:21 +0100

    Adding footsteps SFX to Adamastor

---
+++ Assets/Scripts/Character/CharacterFootstepSFXMaker.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class CharacterFootstepSFXMaker : MonoBehaviour
+{
+    CharacterManager character;
+
+    AudioSource audioSource;
+    GameObject steppedOnObject;
+
+    private bool hasTouchedGround = false;
+    private bool hasPlayedFootstepSFX = false;
+
+    private void Start()
+    {
+        character = GetComponentInParent<CharacterManager>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void CheckForFootSteps()
+    {
+        if (character == null) return;
+
+        if (!character.isMoving) return;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, character.transform.TransformDirection(Vector3.down), out hit, 0.05f, WorldUtilityManager.Instance.GetEnviroLayer()))
+        {
+            hasTouchedGround = true;
+
+            if (!hasPlayedFootstepSFX)
+            {
+                steppedOnObject = hit.transform.gameObject;
+            }
+        }
+        else
+        {
+            hasTouchedGround = false;
+            hasPlayedFootstepSFX = false;
+            steppedOnObject = null;
+        }
+
+        if (hasTouchedGround && !hasPlayedFootstepSFX)
+        {
+            hasPlayedFootstepSFX = true;
+            PlayFootstepSFX();
+        }
+    }
+
+    private void PlayFootstepSFX()
+    {
+        // TODO: ADD LOGIC FOR PLAYING FOOTSTEP SFX BASED ON THE STEPPED ON OBJECT
+
+        //audioSource.PlayOneShot(WorldSoundFXManager.Instance.GetFootstepSFX(steppedOnObject));
+    }
+}
