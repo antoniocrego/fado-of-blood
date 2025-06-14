41e64549b6dd8b613b6f4cb12f04305013f77e57
António Rego <antoniocostarego618@gmail.com>
2025-06-14 14:30:05 +0100

    began work on soldier enemy + main menu fixes

---
+++ Assets/Resources/Enemies/Male Husk/AIMaleHuskCombatManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class AIMaleHuskCombatManager : AICharacterCombatManager
+{
+    [Header("Damage Colliders")]
+    [SerializeField] MaleHuskHandDamageCollider rightHandDamageCollider;
+    [SerializeField] MaleHuskHandDamageCollider leftHandDamageCollider;
+
+    [Header("Damage")]
+    [SerializeField] float baseDamage = 10f;
+    [SerializeField] float attack01DamageMultiplier = 1f;
+    [SerializeField] float attack02DamageMultiplier = 1.5f;
+
+    public void SetAttack01Damage()
+    {
+        // we could have both calculations here, but since each attack only uses 1 hand, its not worth it
+        leftHandDamageCollider.damage = baseDamage * attack01DamageMultiplier;
+    }
+
+    public void SetAttack02Damage()
+    {
+        rightHandDamageCollider.damage = baseDamage * attack02DamageMultiplier;
+    }
+
+    public void OpenRightHandDamageCollider()
+    {
+        rightHandDamageCollider.EnableDamageCollider();
+    }
+
+    public void CloseRightHandDamageCollider()
+    {
+        rightHandDamageCollider.DisableDamageCollider();
+    }
+
+    public void OpenLeftHandDamageCollider()
+    {
+        leftHandDamageCollider.EnableDamageCollider();
+    }
+
+    public void CloseLeftHandDamageCollider()
+    {
+        leftHandDamageCollider.DisableDamageCollider();
+    }
+
+    public override void PivotTowardsTarget(AICharacterManager aiCharacter)
+    {
+        if (aiCharacter.isPerformingAction) return;
+
+        if (viewableAngle >= 40 && viewableAngle <= 110)
+        {
+            aiCharacter.characterAnimatorManager.PlayTargetActionAnimation("Turn 90 R", true);
+        }
+        else if (viewableAngle <= -40 && viewableAngle >= -110)
+        {
+            aiCharacter.characterAnimatorManager.PlayTargetActionAnimation("Turn 90 L", true);
+        }
+
+    }
+}
